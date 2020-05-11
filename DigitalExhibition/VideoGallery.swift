709f04317909cd665911dba3fea17fa62d8414dd
Jakob <18356553@student.westernsydney.edu.au>
2020-05-11 13:39:57 +0300

    * Added local database for the survey

--- DigitalExhibition/VideoGallery.swift
+++ DigitalExhibition/VideoGallery.swift
@@ -10,12 +10,31 @@ import SwiftUI
 
 struct VideoGallery: View {
     var body: some View {
-        Text("Video Gallery and User management")
+        NavigationView {
+            Text("BACKEND")
+            .navigationBarTitle("Video Gallery")
+            //.navigationBarItems(trailing:
+                HStack {
+                    Button("About") {
+                        print("About tapped!")
+                    }
+
+                    Button("Logout") {
+                        print("Help tapped!")
+                    }
+                }
+            //)
+        }
     }
 }
 
 struct VideoGallery_Previews: PreviewProvider {
     static var previews: some View {
         VideoGallery()
+        /*.previewDevice("iPad mini 4")
+        .previewLayout(
+            PreviewLayout.fixed(
+                width: 2732.0,
+                height: 2048.0))*/
     }
 }
